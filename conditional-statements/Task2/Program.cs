0be949a45857428c1b0799b9f2f5dbc8ad977add
MikaPaakki <mika.paakki@student.saimia.fi>
2017-09-12 14:01:20 +0300

    Conditional statement task2 was made

--- conditional-statements/Task2/Program.cs
+++ conditional-statements/Task2/Program.cs
@@ -4,9 +4,38 @@ namespace Task2
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    
+        
+            static void Main(string[] args)
+            {
+                // Prompt user
+                Console.Write("Please enter a number:");
+
+                // Read use input
+                string userInput;
+                userInput = Console.ReadLine();
+
+                // Evaluate user input
+
+                int evaluatedNumber;
+                int.TryParse(userInput, out evaluatedNumber);
+
+            
+                if (evaluatedNumber %2 ==0)
+
+                // Number 0 is considered to be even
+                {
+                    Console.WriteLine("Number {0} is even", evaluatedNumber);
+                }
+
+                else
+                {
+                    Console.WriteLine("Number {0} is odd", evaluatedNumber);
+                }
+
+         
+                // Wait for user input
+                Console.ReadKey();
+            }
         }
     }
-}
