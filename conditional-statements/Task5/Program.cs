f7f5e95becae6eba9797bce636c0a4fdb912b158
MikaPaakki <mika.paakki@student.saimia.fi>
2017-09-13 15:03:13 +0300

    Loop statement

---
+++ conditional-statements/Task5/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Please add your age");
+
+            float ticketPrice = 100.0f;
+            float discount = 0.0f;
+            // Read use input
+            string userInput;
+            userInput = Console.ReadLine();
+            int age;
+            int.TryParse(userInput, out age);
+            // Evaluate user input
+
+            // Prompt user
+            Console.WriteLine("Please add your status [1-4]");
+            Console.WriteLine("1. Eläkeläinen");
+            Console.WriteLine("2. Varusmies");
+            Console.WriteLine("3. Opiskelija");
+            Console.WriteLine("4. joku muu");
+
+            // Prompt user
+            Console.WriteLine("Are you member of MTK, yes/no [1-2]");
+            Console.WriteLine("5.Yes");
+            Console.WriteLine("6.No");
+        
+            // Read use input
+            userInput = Console.ReadLine();
+
+            // Evaluate user input
+
+           
+
+            int status;
+            int.TryParse(userInput, out status);
+
+            //IF < 7
+            if (age > 7)
+            {
+                Console.WriteLine("Number {0} is <7", age);
+            }
+            //IF >7
+            else if (age > 7)
+            {
+                Console.WriteLine("Number {0} is <7", age);
+            }
+            //IF <15
+            else if (age < 15)
+            {
+                Console.WriteLine("Number {0} is <15", age);
+            }
+            //IF ==65
+            if (age == 65)
+            {
+                Console.WriteLine("Number {0} is ==65", age);
+            }
+            //IF >65
+            else if (age > 65)
+            {
+                Console.WriteLine("Number {0} is >65", age);
+            }
+
+            if(status == 1)
+            {
+                discount = 0.5f;
+            }
+            else if(status == 2)
+            {
+                discount = 0.5f;
+            }
+            else if(status == 3)
+            {
+                discount = 0.45f;
+            }
+            else
+            {
+                discount = 1.0f;
+            }
+            
+            float totPrice = ticketPrice - (ticketPrice * discount);
+
+            Console.WriteLine("Your ticket price is {0} e", totPrice);
+            Console.ReadKey();
+        }
+
+    }
+}
+
---
+++ loop-statements/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate factorial, please type a number");
+            int factorial = 1;
+            int i = 1;
+            // Read use input
+            string userInput;
+            userInput = Console.ReadLine();
+
+            // Evaluate user input
+
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+            for (i = 1; i <= evaluatedNumber; i++)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine("{0}!={1}", evaluatedNumber, factorial);
+            Console.ReadKey();
+        }
+    }
+}
