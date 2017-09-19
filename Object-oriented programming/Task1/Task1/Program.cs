52c1f4cf593770cd724974a3779b3c4926f4f985
MikaPaakki <mika.paakki@student.saimia.fi>
2017-09-19 13:22:02 +0300

    Object oriented programming folder

---
+++ Object-oriented programming/Task1/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            
+
+           
+
+            
+            Console.ReadLine();
+        }
+    }
+}
