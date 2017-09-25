a18d99574fd86ab78e5e44c14ee034f7153cc2ed
MikaPaakki <mika.paakki@student.saimia.fi>
2017-09-25 12:58:09 +0300

    Method statements folder

---
+++ method-statements/method-statements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace method_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
