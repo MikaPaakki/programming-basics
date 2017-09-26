cb4472f310a7a6e238290de58be4f1d1413e1300
MikaPaakki <mika.paakki@student.saimia.fi>
2017-09-26 14:57:28 +0300

    Method statements task 2 made, task3 started

---
+++ method-statements/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Return minimum value, please type two numbers");
+            Console.Write("type number 1/2: ");
+            int numberA;
+            string userInput = Console.ReadLine();
+            numberA = int.Parse(userInput);
+
+            Console.Write("type number 2/2: ");
+            int numberB;
+            userInput = Console.ReadLine();
+            numberB = int.Parse(userInput);
+
+           Console.WriteLine($"Syotetyistä luvuista pienempi on {Minimum(numberA,numberB)}");
+            
+            Console.ReadKey();
+        }
+
+        static int Minimum(int numberX, int numberY)
+        {
+
+
+            // ehto ? true_value : false_value;
+
+            return (numberX < numberY) ? numberX : numberY; 
+
+            /*if (numberX < numberY)
+                return numberX;
+            else
+                return numberY;
+                */
+        }
+    }
+
+}
+    
---
+++ method-statements/Task3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //type number between "x-xx"
+            //if not between "x-xx", number is invalid
+            //if type is not a number, type is invalid
+            //method returns typed number
+        {
+            /*
+            Console.WriteLine("Return value, please type number between x-xx");
+            Console.Write("Type number: ");
+            int number;
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+            if (number > 0)
+            {
+                Console.WriteLine($"{(number)}");
+            }
+            else
+            {
+                Console.WriteLine($"Number {number} is invalid");
+            }
+            
+            Console.WriteLine($"Text)");
+            */
+            int MinimNumber = 1;
+            int MaximNumber = 50;
+            int result = NumberChecker(MinimNumber, MaximNumber);
+
+            Console.ReadKey();
+        }
+
+        static int NumberChecker(int LowerLimit, int UpperLimit)
+        {
+
+            bool isNumberAcceptable = false;
+
+            //DO
+
+            // Prompt user
+
+            // Read and parse userinput (remember int.TryParse(...) returns parse result, this could be useful)
+            int.TryParse(...)
+            // IF ! parse success
+            // Console.Clear();
+            // Check keyword "continue" functionality
+            continue;
+            // Check user input
+
+            // IF acceptable number
+
+            // isNumberAcceptable = true;
+
+            // ELSE
+
+            // isNumberAcceptable = false;
+            // Console.Clear();
+
+            //WHILE(!isNumberAcceptable)
+
+            //Return acceptable number
+
+        }
+    }
+}
