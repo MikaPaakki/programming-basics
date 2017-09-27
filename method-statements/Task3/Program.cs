b39d2339ccb0622b3291766ba11bca3ce8241f06
MikaPaakki <mika.paakki@student.saimia.fi>
2017-09-27 14:11:44 +0300

    Method statements tasks 3 and 4 was finished

--- method-statements/Task3/Program.cs
+++ method-statements/Task3/Program.cs
@@ -10,60 +10,25 @@ namespace Task3
             //if type is not a number, type is invalid
             //method returns typed number
         {
-            /*
-            Console.WriteLine("Return value, please type number between x-xx");
-            Console.Write("Type number: ");
-            int number;
-            string userInput = Console.ReadLine();
-            number = int.Parse(userInput);
-            if (number > 0)
-            {
-                Console.WriteLine($"{(number)}");
-            }
-            else
-            {
-                Console.WriteLine($"Number {number} is invalid");
-            }
-            
-            Console.WriteLine($"Text)");
-            */
             int MinimNumber = 1;
             int MaximNumber = 50;
             int result = NumberChecker(MinimNumber, MaximNumber);
-
+            Console.WriteLine("You did input number{0}", result);
             Console.ReadKey();
         }
 
         static int NumberChecker(int LowerLimit, int UpperLimit)
         {
-
-            bool isNumberAcceptable = false;
-
-            //DO
-
-            // Prompt user
-
-            // Read and parse userinput (remember int.TryParse(...) returns parse result, this could be useful)
-            int.TryParse(...)
-            // IF ! parse success
-            // Console.Clear();
-            // Check keyword "continue" functionality
-            continue;
-            // Check user input
-
-            // IF acceptable number
-
-            // isNumberAcceptable = true;
-
-            // ELSE
-
-            // isNumberAcceptable = false;
-            // Console.Clear();
-
-            //WHILE(!isNumberAcceptable)
-
-            //Return acceptable number
-
+            
+            int number;
+            Console.WriteLine("Please enter a number between {0} and {1}.", LowerLimit, UpperLimit);
+            string userInput = Console.ReadLine();
+            while(!int.TryParse(userInput, out number) || number < LowerLimit || number >UpperLimit)
+            {
+                Console.WriteLine("number is invalid, please enter a number between {0} and {1}.", LowerLimit, UpperLimit);
+                userInput = Console.ReadLine();
+            }
+            return number;
         }
     }
 }
---
+++ method-statements/Task4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //type ten numbers
+            //if not positive, number is invalid, enter new number
+            //return value of the greatest number
+        {
+
+            NumberOrder();
+            Console.ReadKey();
+            
+        }
+
+        static void NumberOrder()
+        {
+            Console.WriteLine("Write ten numbers");
+
+            List<int> numbers = new List<int>();
+
+            int counter = 1;
+            while (counter <= 10)
+            {
+                //Console.Write("Write numberA?");
+                Console.Write($"Write number {counter}: ");
+                /*int number;
+                string userInput = Console.ReadLine();
+                number = int.Parse(userInput);*/
+                int numberA;
+                string userInput = Console.ReadLine();
+                numberA = int.Parse(userInput);
+                if (numberA > 0)
+                {
+                    //Console.WriteLine($"{counter}. {(numberA)}");
+                    numbers.Add(numberA);
+                    counter = counter + 1;
+
+
+                }
+                else
+                {
+                    Console.WriteLine($"Number {numberA} is invalid, please enter a positive number");
+                }
+            }
+
+            Console.WriteLine("You added these numbers");
+            foreach (int number in numbers)
+            {
+                Console.Write($"{number} ");
+            }
+
+            int maxNumber = numbers.Max();
+            int maxNumberIndex = numbers.IndexOf(maxNumber) + 1;
+
+            Console.WriteLine();
+            Console.WriteLine($"Maximum number was {maxNumber}, {maxNumberIndex}. number");
+        }
+    }
+}
