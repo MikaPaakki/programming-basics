33dc17f994bd47b2926241ed948afdb6f9ae1ec6
MikaPaakki <mika.paakki@student.saimia.fi>
2017-09-27 14:55:08 +0300

    String statements task1 started.

---
+++ string-statements/string-statements/string-statements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace string_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
